4c40ed4ad47c98338fd7a52eb9ccc1876cb88e1c
KamleshPatel98 <kamleshp52170@gmail.com>
2026-06-03 21:58:12 +0530

    user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if(User::count() == 0){
+            User::create([
+                'name' => 'Admin',
+                'mobile' => '1234567891',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('password1@'),
+            ]);
+        }
+    }
+}
