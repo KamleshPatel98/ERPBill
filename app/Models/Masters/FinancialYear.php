e4fb355fbe037ec368239e73b8eac354b86c73fb
KamleshPatel98 <kamleshp52170@gmail.com>
2026-06-04 16:50:25 +0530

    date format in financial year model

--- app/Models/Masters/FinancialYear.php
+++ app/Models/Masters/FinancialYear.php
@@ -2,9 +2,36 @@
 
 namespace App\Models\Masters;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class FinancialYear extends Model
 {
     protected $fillable = ['name', 'start_date', 'end_date', 'is_active'];
+
+    protected function startDate(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value
+                ? date('d-m-Y', strtotime($value))
+                : null,
+
+            set: fn ($value) => $value
+                ? date('Y-m-d', strtotime($value))
+                : null,
+        );
+    }
+
+    protected function endDate(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value
+                ? date('d-m-Y', strtotime($value))
+                : null,
+
+            set: fn ($value) => $value
+                ? date('Y-m-d', strtotime($value))
+                : null,
+        );
+    }
 }
