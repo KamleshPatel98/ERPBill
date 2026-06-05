91181f36f89de7dbe58f3b3664394b0801f58877
KamleshPatel98 <kamleshp52170@gmail.com>
2026-06-05 19:57:03 +0530

    purchase table create

---
+++ app/Models/Purchase.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Masters\Supplier;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+
+class Purchase extends Model
+{
+    protected $fillable = [
+        'invoice_no',
+        'invoice_date',
+        'supplier_id',
+        'financial_year_id',
+        'payment_mode_id',
+        'payment_status',
+        'grand_amount',
+        'gst_amount',
+        'discount_amount',
+        'total_amount',
+        'paid_amount',
+        'due_amount',
+        'notes',
+    ];
+
+    protected function invoiceDate(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value
+                ? date('d-m-Y', strtotime($value))
+                : null,
+
+            set: fn ($value) => $value
+                ? date('Y-m-d', strtotime($value))
+                : null,
+        );
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function financialYear()
+    {
+        return $this->belongsTo('FinancialYear');
+    }
+
+    public function paymentMode()
+    {
+        return $this->belongsTo('PaymentMode');
+    }
+}
