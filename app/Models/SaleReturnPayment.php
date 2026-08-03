a3bcb0a1cb60995afb3b3f50f0965db318263ba6
KamleshPatel98 <kamleshp52170@gmail.com>
2026-08-03 17:59:43 +0530

    sale return payment table create

---
+++ app/Models/SaleReturnPayment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Masters\PaymentMode;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+
+class SaleReturnPayment extends Model
+{
+    protected $fillable = ['sale_return_id', 'payment_mode_id', 'date', 'amount'];
+
+    protected function date(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value
+                ? date('d-m-Y', strtotime($value))
+                : null,
+
+            set: fn ($value) => $value
+                ? date('Y-m-d', strtotime($value))
+                : null,
+        );
+    }
+
+    public function paymentMode()
+    {
+        return $this->belongsTo(PaymentMode::class);
+    }
+}
